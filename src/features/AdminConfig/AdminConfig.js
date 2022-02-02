6098741899411e204de8a589bd593db5a1532c5e
Sean Mizen <sean@seanmizen.com>
2022-02-02 09:56:33 +0000

    Added dropdown example to AdminConfig

--- src/features/AdminConfig/AdminConfig.js
+++ src/features/AdminConfig/AdminConfig.js
@@ -1,4 +1,45 @@
 import styles from "./AdminConfig.module.css";
+import TourPicker from "./components/TourPicker";
+
+/* This will be stored somewhere proper, eventually. for now, use this to build the site */
+const tourList = [
+  {
+    name: "Headington Campus",
+    id: 123,
+    waypoints: [
+      {
+        id: 1,
+        name: "JHB Ramp",
+        latitude: 51.755291,
+        longditude: -1.225515,
+      },
+      {
+        id: 2,
+        name: "JHB Reception",
+        latitude: 51.755,
+        longditude: -1.22525,
+      },
+    ],
+  },
+  {
+    name: "Wheatley Campus",
+    id: 321,
+    waypoints: [
+      {
+        id: 1,
+        name: "PG101",
+        latitude: 51.7494,
+        longditude: -1.127831,
+      },
+      {
+        id: 2,
+        name: "Dora Cohen",
+        latitude: 51.74894,
+        longditude: -1.1268,
+      },
+    ],
+  },
+];
 
 function AdminConfig() {
   const f = (a, b) => {
@@ -7,7 +48,10 @@ function AdminConfig() {
   };
 
   return (
-    <div className={styles["stage"]}>{f(4, 2) + " - this is a template"}</div>
+    <div className={styles[""]}>
+      <p>{f(4, 2) + " - this is a template"}</p>
+      <TourPicker tourList={tourList} />
+    </div>
   );
 }
 
