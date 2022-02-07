ad461bdd855b4d0b77aeea6ea17b4e58c0e10a9b
Sean Mizen <sean@seanmizen.com>
2022-02-07 22:14:52 +0000

    added home comments

--- src/features/Home/Home.js
+++ src/features/Home/Home.js
@@ -1,7 +1,15 @@
 import styles from "./Home.module.css";
 
 function Home() {
-  return <div className={styles[""]}>home</div>;
+  return (
+    <div className={styles[""]}>
+      <p>home</p>
+      <p>
+        handy hint for devs: with the "stage" scene - control+option+i will open
+        up a debug inspector. (ctrl+alt+i in windows)
+      </p>
+    </div>
+  );
 }
 
 export default Home;
