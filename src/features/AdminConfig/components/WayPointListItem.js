eca3ebc808aff6c53d56157f08b087343c61d2f7
Philip Borowiak <heyhej19@gmail.com>
2022-02-12 11:19:29 +0000

    Edit option still inprogress

--- src/features/AdminConfig/components/WayPointListItem.js
+++ src/features/AdminConfig/components/WayPointListItem.js
@@ -2,10 +2,10 @@ import styles from "../AdminConfig.module.css";
 function WayPointListItem({ id, name, latitude, longditude }) {
   return (
     <li>
-      <span className={styles[""]}>{id}</span>
-      <span className={styles[""]}>{name}</span>
-      <span className={styles[""]}>{latitude}</span>
-      <span className={styles[""]}>{longditude}</span>
+      <span className={styles["id-style"]}>{id}</span>
+      <span className={styles["name-style"]}>{name}</span>
+      <span className={styles["latitude-style"]}>{latitude}</span>
+      <span className={styles["longditude-style"]}>{longditude}</span>
     </li>
   );
 }
--- src/features/AdminConfig/components/WayPointListItemEdit.js
+++ src/features/AdminConfig/components/WayPointListItemEdit.js
@@ -1,6 +1,6 @@
 import styles from "../AdminConfig.module.css";
-import { useState, useRef } from "react";
-function WayPointListItemEdit({ name, latitude, longditude }) {
+import { useState, useRef, useEffect } from "react";
+function WayPointListItemEdit({ name, latitude, longditude,editItemCallback }) {
   const [innerName, setInnerName] = useState(name);
   const [innerLatitude, setInnerLatitude] = useState(latitude);
   const [innerLongditude, setInnerLongditude] = useState(longditude);
@@ -9,36 +9,85 @@ function WayPointListItemEdit({ name, latitude, longditude }) {
   const editNameRef = useRef();
   const editLatitudeRef = useRef();
   const editLongditudeRef = useRef();
+  const liRef = useRef();
+
+useEffect(() => {
+  editNameRef.current.focus();
+  editLatitudeRef.current.focus();
+  editLongditudeRef.current.focus();
+
+}, [name, latitude, longditude]);
+
+  const keyDown = (e) => {
+    if (e.keyCode === 13 && e.shiftKey === false) {
+      submitRef.current.click();
+    }
+    if (e.keyCode === 27) {
+      cancelRef.current.click();
+    }
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    editItemCallback({ name: name, latitude: latitude, longditude: longditude });
+    
+  };
+  const cancelSubmit = (e) => {
+    e.preventDefault();
+    setInnerName(name);
+    setInnerLatitude(latitude);
+    setInnerLongditude(longditude);
+  };
+
+   //handle doubleclicks
+   useEffect(() => {
+    const handleDoubleClick = (e) => {
+      console.log(e.composedPath().includes(liRef.current));
+      if (e.composedPath().includes(liRef.current)) {
+          cancelSubmit(e);
+        }
+      };
+
+    window.addEventListener("dblclick", handleDoubleClick);
+
+    //stop the app from generating a bajillion eventListeners:
+    return () => {
+      window.removeEventListener("dblclick", handleDoubleClick);
+    };
+  });
+ 
+  
 
   return (
     <li>
-      <form>
+      <form onSubmit={submitForm} onReset={cancelSubmit} onKeyDown={keyDown} >
         <input
-          className={styles[""]}
+          className={styles["name-edit"]}
           value={innerName}
           required={true}
           onChange={(e) => setInnerName(e.target.value)}
           ref={editNameRef}
         />
         <input
-          className={styles[""]}
+          className={styles["latitude-edit"]}
           value={innerLatitude}
           onChange={(e) => setInnerLatitude(e.target.value)}
           ref={editLatitudeRef}
         />
         <input
-          className={styles[""]}
+          className={styles["longditude-edit"]}
           value={innerLongditude}
           onChange={(e) => setInnerLongditude(e.target.value)}
           ref={editLongditudeRef}
         />
-
+        <div className={styles["edit-buttons"]}>
         <button type="submit" ref={submitRef}>
           Save
         </button>
         <button type="reset" ref={cancelRef}>
           Cancel
         </button>
+        </div>
       </form>
     </li>
   );
